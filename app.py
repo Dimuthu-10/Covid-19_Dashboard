a2f226b6134c7ba0fed547d022ee6138634623a4
Hewarathna Ashen Iranga <48565752+ashen007@users.noreply.github.com>
2021-06-07 23:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import webbrowser
+# import webbrowser
 
 st.header('welcome to our dashboard !!!')
 st.subheader('''
@@ -15,7 +15,7 @@ covid-19 situation report
 
 
 
-url = 'https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D'
+[to dashboard]('https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D')
 
-if st.button('Open browser'):
-    webbrowser.open_new_tab(url)
+# if st.button('Open browser'):
+#     webbrowser.open_new_tab(url)
