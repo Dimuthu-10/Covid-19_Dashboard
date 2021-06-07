b7cc72700131365cc064092f67bc7b6ffabd31c3
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-06-08 00:20:52 +0530

    updated quation mark

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ options = st.selectbox('select mode:', ['PowerBI'])
 
 if options == 'PowerBI':
     st.markdown(
-        "<iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>",
+        '''<iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>''',
          unsafe_allow_html=True)
 
 # st.write("[to_dashboard](https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D)")
