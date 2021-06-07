1506830da8edae4069728067a148a4510257598d
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-06-08 00:16:23 +0530

    add iframe

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ st.subheader('''
 covid-19 situation report
 ''')
 
-# options = st.selectbox('select mode:', ['PowerBI', 'Preprocessing & predictions'])
+options = st.selectbox('select mode:', ['PowerBI'])
 
-# if options == 'PowerBI':
-#     st.markdown(
-#         "https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D",
-#         unsafe_allow_html=True)
+if options == 'PowerBI':
+    st.markdown(
+        "<iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>",
+         unsafe_allow_html=True)
 
-st.write("[to_dashboard](https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D)")
+# st.write("[to_dashboard](https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D)")
 
 # to_dashboard](https://app.powerbi.com/reportEmbed?reportId=ce47b75d-7856-48e3-bf6c-da64e4d4503b&autoAuth=true&ctid=49211f0f-e082-499f-94fb-96dff1601a14&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D)
 
